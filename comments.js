52d63a47b862900568d96d69e23a44fd88c0098f
Vij Patel <30951833+vijpatel@users.noreply.github.com>
2023-07-22 17:46:01 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,7 @@
-const http = require('http');
-
-// Create a server object
-const server = http.createServer((req, res) => {
-  // Set the response header
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  // Write the response content
-  res.end('Hello, World!');
-});
-
-// Set the port the server will listen on
-const port = 3000;
-
-// Start the server and listen for incoming requests
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}/`);
-});
+// Create web server
+// 1. load modules
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+// 2. create web server
